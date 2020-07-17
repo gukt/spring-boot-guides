b52caa509ace3a8d2dfe177797c98cb79211c4b3
gukt <29283212@qq.com>
2020-07-17 18:50:14 +0800

    Initial Commit

---
+++ sbg-spring-boot-scaffold/src/main/java/tech/codedog/guides/SbgSpringBootScaffoldApplication.java
@@ -0,0 +1,13 @@
+package tech.codedog.guides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbgSpringBootScaffoldApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbgSpringBootScaffoldApplication.class, args);
+	}
+
+}
