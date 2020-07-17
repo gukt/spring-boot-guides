f834100368f6bf60ec4e1f592db8c49bea890e01
gukt <29283212@qq.com>
2020-07-17 18:58:46 +0800

    Initail Commit

---
+++ sbg-scaffold/src/test/java/tech/codedog/guides/SbgScaffoldApplicationTests.java
@@ -0,0 +1,13 @@
+package tech.codedog.guides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbgScaffoldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
